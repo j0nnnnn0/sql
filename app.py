997323fababaadff7f10c18eab765295e1e1d4f9
j0nnnnn0 <j0nnnnn0@gmail.com>
2023-08-27 14:53:08 +0200

    update again

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
 from faker import Faker
-from sqlalchemy import ForeignKey, String, create_engine,select
+from sqlalchemy import ForeignKey, String, create_engine,select,func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, Session
 
 
@@ -124,4 +124,19 @@ print(results)
 # get all customers from the US
 query = select(Customer).where(Customer.country_code == "BR")
 results = session.execute(query).scalars().all()
+print(results)
+
+# select a list of countries grouped by country code
+query = select(Customer.country_code, func.count(Customer.country_code)).group_by(Customer.country_code)
+results = session.execute(query).all()
+print(results)
+
+# select customer name and credit card number
+query = select(Customer.name, CreditCard.number).join(CreditCard)
+results = session.execute(query).all()
+print(results)
+
+# select customer name with their number of orders
+query = select(Customer.name, func.count(Order.id)).join(Order).group_by(Customer.name)
+results = session.execute(query).all()
 print(results)
